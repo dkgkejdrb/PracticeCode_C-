040577f6d9c47ec0f49c78356c967f1c6616f9f9
dkgkejdrb <70058394+dkgkejdrb@users.noreply.github.com>
2022-07-02 18:28:23 +0900

    struct와 class마지막비교

---
+++ immutable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace immutable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string res = String.Empty;
+			for (int i = 0; i < 100; i++)
+			{
+				res += "Hello";
+			}
+			Console.WriteLine(res);
+		}
+	}
+}
---
+++ immutableStruct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace immutableStruct
+{
+	struct add
+	{
+		public string join(string a, int b)
+		{
+			string res = String.Empty;
+			for (int i = 0; i < b; i++)
+			{
+				res += a;
+			}
+			return res;
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var obj = new add();
+			Console.WriteLine(obj.join("Hello", 100));
+		}
+	}
+}
