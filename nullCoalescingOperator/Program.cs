93add5cefd9edd3c607b5b5edc217949a0fb4602
dkgkejdrb <70058394+dkgkejdrb@users.noreply.github.com>
2022-07-03 13:08:21 +0900

    nullCoalescingOperator 추가

---
+++ nullCoalescingOperator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace nullCoalescingOperator
+{
+	class Greeter
+	{
+		internal string Greet1(string name)
+		{
+			if (name == null)
+			{
+				return "Hello, stranger!";
+			}
+			return $"Hello, {name}";
+		}
+
+		internal string Greet2(string name)
+		{
+			return $"Hello, {name ?? "stranger"}!";
+		}
+	}
+
+	internal class Program
+	{
+		private static List<int> _numbers;
+
+		private static void AddToNumbers(int number)
+		{
+			//if(_numbers == null)
+			//{
+			//	_numbers = new List<int>();
+			//}
+			//_numbers.Add(number);
+			
+			(_numbers ??= new List<int>()).Add(number); // ??= 추가
+		}
+
+		private static void ClearIfNotNull(List<int> numbers)
+		{
+			numbers?.Clear(); // ?. 추가
+		}
+
+		static void Main(string[] args)
+		{
+			//var obj = new Greeter();
+			//WriteLine(obj.Greet1(null));
+			//Write(obj.Greet1("이동규"));
+
+			//var obj = new Greeter();
+			//WriteLine(obj.Greet2(null));
+			//WriteLine(obj.Greet2("이동규"));
+
+			//AddToNumbers(10);
+			//WriteLine(_numbers.Count);
+
+			//AddToNumbers(20);
+			//WriteLine(_numbers.Count);
+
+			ClearIfNotNull(_numbers);
+			WriteLine(_numbers.Count);
+		}
+	}
+}
---
+++ nullCoalescingOperator1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace nullCoalescingOperator1
+{
+	class Person
+	{
+		public string Name;
+		public string LastName;
+		int Age;
+	}
+
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			PrintName(new Person { Name = "동규", LastName = "이" });
+			PrintName(new Person { LastName = "정" });
+			PrintName(new Person());
+		}
+
+		static void PrintName(Person person)
+		{
+			WriteLine(person.Name ?? person.LastName ?? "stranger");
+		}
+
+	}
+}
