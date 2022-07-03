ec86c703fe1cf6239ce6409e88753a3ef405f41f
dkgkejdrb <70058394+dkgkejdrb@users.noreply.github.com>
2022-07-03 10:59:56 +0900

    staticClass/Constructor 예시 코드

---
+++ StaticClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console; // 추가
+
+namespace StaticClass
+{
+	static class Calculator // 컨테이너 기능
+	{
+		public static double add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public static double mul(double a, double b)
+		{
+			return a * b;
+		}
+
+		public static double power(double a)
+		{
+			return a * a;
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//WriteLine(Calculator.add(5, 2.5));
+			//WriteLine(Calculator.mul(5, 2.5));
+			//WriteLine(Calculator.power(5));
+
+			WriteLine(SystemMonitor.Report());
+		}
+	}
+}
---
+++ StaticClass/Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticClass
+{
+	static class SystemMonitor
+	{
+		private static readonly DateTime _startTime;
+		
+		static SystemMonitor() // static constructor
+		{
+			_startTime = DateTime.UtcNow;
+		}
+
+		public static string Report()
+		{
+			return $"{(DateTime.UtcNow - _startTime).TotalSeconds} seconds";
+		}
+	}
+}
