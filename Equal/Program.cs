fc218ada85c5aaf06c30173cf6a86faf97e6b8f6
dkgkejdrb <70058394+dkgkejdrb@users.noreply.github.com>
2022-07-05 20:31:54 +0900

    == 연산자와 Equals() 메소드 비교

---
+++ Equal/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Equal
+{
+	//class CustomClass
+	//{
+	//	int X;
+	//	public CustomClass(int x)
+	//	{
+	//		X = x;
+	//	}
+	//}
+
+	class CustomClassOverridingEquals
+	{
+		int X;
+		public CustomClassOverridingEquals(int x)
+		{
+			X = x;
+		}
+
+		// Equals() 오버라이드
+		public override bool Equals(object obj) 
+		{
+			var item = obj as CustomClassOverridingEquals;
+
+			if (item == null)
+			{
+				return false;
+			}
+
+			return X == item.X;
+		}
+
+		// Equalis() 오버라이드 하기 위해 필요한 메소드
+		public override int GetHashCode()
+		{
+			return X.GetHashCode();
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//var customClass1 = new CustomClass(1);
+			//var customClass2 = new CustomClass(1);
+			//Console.WriteLine($"customClass1 == customClass2: " +
+			//	$"{customClass1 == customClass2}"); // false
+			//Console.WriteLine($"customClass1.Equals(customClass2): " +
+			//	$"{customClass1.Equals(customClass2)}"); // false
+
+			var customClass1 = new CustomClassOverridingEquals(1);
+			var customClass2 = new CustomClassOverridingEquals(1);
+			Console.WriteLine($"customClass1 == customClass2: " +
+				$"{customClass1 == customClass2}"); // true
+			Console.WriteLine($"customClass1.Equals(customClass2): " +
+				$"{customClass1.Equals(customClass2)}"); // false
+		}
+	}
+}
---
+++ EqualValueType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EqualValueType
+{
+	struct CustomStruct
+	{
+		int X;
+
+		public CustomStruct(int x)
+		{
+			X = x;
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var customStruct1 = new CustomStruct(1);
+			var customStruct2 = new CustomStruct(1);
+			//Console.WriteLine($"customStruct1 == customStruct2: " +
+			//	$"{customStruct1 == customStruct2}"); // error
+			Console.WriteLine($"customStruct1.Equals(customStruct2): " +
+				$"{customStruct1.Equals(customStruct2)}"); // true
+		}
+	}
+}
