f9729240de1b18adbdcfe5517d20ffbb43f06a25
dkgkejdrb <70058394+dkgkejdrb@users.noreply.github.com>
2022-07-06 20:55:02 +0900

    nullable type 예시코드

---
+++ NuallableType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NuallableType
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// int Height = null; // value type 에 null 값을 할당할 수 없음, 에러
+			int? Height = null;
+			Nullable<int> Height2 = null;
+			Nullable<int> Height3 = new Nullable<int>();
+
+			if (Height.HasValue)
+			{
+				Console.WriteLine($"Height is {Height.Value}");
+			}
+			else
+			{
+				Console.WriteLine($"Height is null");
+			}
+
+			int number = Height.Value; // 정수에 null value를 할당할 수 있음
+		}
+	}
+}
